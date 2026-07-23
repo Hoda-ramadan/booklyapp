23fe58896a053dd462ebfaf4add8fae94d431145
Huda Ramadan <011hodaramadan@gmail.com>
2026-07-23 14:58:59 -0700

    add custom list view item

--- lib/features/home/date/presentation/view/widget/home_view_body.dart
+++ lib/features/home/date/presentation/view/widget/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/features/home/date/presentation/view/widget/customAppbar.dart';
+import 'package:bookly_app/features/home/date/presentation/view/widget/customlistviewItem.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -6,6 +7,6 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [Customappbar()]);
+    return Column(children: [Customappbar(), CustomListViewItem()]);
   }
 }
--- lib/utalts/assets.dart
+++ lib/utalts/assets.dart
@@ -1,3 +1,4 @@
 class Assets {
   static const logoimagpath = "assets/images/Logo.png";
+  static const testimage = "assets/images/test_image.png";
 }
