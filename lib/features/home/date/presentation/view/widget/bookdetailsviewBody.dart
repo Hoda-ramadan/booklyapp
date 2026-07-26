1477a177001c5743ba669687a4afe8d85638b7c9
Huda Ramadan <011hodaramadan@gmail.com>
2026-07-26 09:42:29 -0700

    add custom book item to book details view

--- lib/features/home/date/presentation/view/widget/bookdetailsviewBody.dart
+++ lib/features/home/date/presentation/view/widget/bookdetailsviewBody.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/features/home/date/presentation/view/widget/custombookdetailsappbar.dart';
+import 'package:bookly_app/features/home/date/presentation/view/widget/feturelistviewitem.dart';
 import 'package:flutter/material.dart';
 
 class Bookdetailsviewbody extends StatelessWidget {
@@ -6,9 +7,18 @@ class Bookdetailsviewbody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(children: [customBookdetailsAppbar()]),
+      child: Column(
+        children: [
+          customBookdetailsAppbar(),
+          Padding(
+            padding: EdgeInsetsGeometry.symmetric(horizontal: width * .2),
+            child: Feturelistviewitem(),
+          ),
+        ],
+      ),
     );
   }
 }
