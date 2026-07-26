bb5b777dbef080dc4ec3303bc4058bf235047c6f
Huda Ramadan <011hodaramadan@gmail.com>
2026-07-26 16:52:35 -0700

    create samaller list view

--- lib/features/home/date/presentation/view/widget/bookdetailsviewBody.dart
+++ lib/features/home/date/presentation/view/widget/bookdetailsviewBody.dart
@@ -2,8 +2,10 @@ import 'package:bookly_app/const.dart';
 import 'package:bookly_app/core/utalts/style.dart';
 import 'package:bookly_app/features/home/date/presentation/view/widget/bookrating.dart';
 import 'package:bookly_app/features/home/date/presentation/view/widget/booksaction.dart';
+import 'package:bookly_app/features/home/date/presentation/view/widget/customListview.dart';
 import 'package:bookly_app/features/home/date/presentation/view/widget/custombookdetailsappbar.dart';
 import 'package:bookly_app/features/home/date/presentation/view/widget/feturelistviewitem.dart';
+import 'package:bookly_app/features/home/date/presentation/view/widget/samallerbooklistview.dart';
 import 'package:flutter/material.dart';
 
 class Bookdetailsviewbody extends StatelessWidget {
@@ -14,36 +16,54 @@ class Bookdetailsviewbody extends StatelessWidget {
     var width = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        children: [
-          customBookdetailsAppbar(),
-          Padding(
-            padding: EdgeInsetsGeometry.symmetric(horizontal: width * .2),
-            child: Feturelistviewitem(),
-          ),
-          SizedBox(height: 46),
-          Text(
-            "The Jungle Book ",
-            style: Style.textstyle30.copyWith(
-              fontFamily: kGTsectrafine,
-              fontWeight: FontWeight.normal,
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(child: customBookdetailsAppbar()),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsetsGeometry.symmetric(horizontal: width * .2),
+              child: Feturelistviewitem(),
             ),
           ),
-          SizedBox(height: 6),
-          Opacity(
-            opacity: .8,
+          SliverToBoxAdapter(child: SizedBox(height: 48)),
+          SliverToBoxAdapter(
             child: Text(
               "The Jungle Book ",
-              style: Style.textstyle18.copyWith(
-                fontWeight: FontWeight.w500,
-                fontStyle: FontStyle.italic,
+              style: Style.textstyle30.copyWith(
+                fontFamily: kGTsectrafine,
+                fontWeight: FontWeight.normal,
               ),
             ),
           ),
-          SizedBox(height: 18),
-          Bookrating(mainAxisAlignment: MainAxisAlignment.center),
-          SizedBox(height: 37),
-          Booksaction(),
+          SliverToBoxAdapter(child: SizedBox(height: 6)),
+          SliverToBoxAdapter(
+            child: Opacity(
+              opacity: .8,
+              child: Text(
+                "The Jungle Book ",
+                style: Style.textstyle18.copyWith(
+                  fontWeight: FontWeight.w500,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(child: SizedBox(height: 18)),
+          SliverToBoxAdapter(
+            child: Bookrating(mainAxisAlignment: MainAxisAlignment.center),
+          ),
+          SliverToBoxAdapter(child: SizedBox(height: 37)),
+          SliverToBoxAdapter(child: Booksaction()),
+          SliverToBoxAdapter(child: SizedBox(height: 50)),
+          SliverToBoxAdapter(
+            child: Text(
+              "you can also like ",
+              style: Style.textstyle14.copyWith(fontWeight: FontWeight.w600),
+            ),
+          ),
+          SliverToBoxAdapter(child: SizedBox(height: 16)),
+          SliverToBoxAdapter(child: SimillerListviewBooks()),
+          SliverToBoxAdapter(child: SizedBox(height: 40)),
         ],
       ),
     );
---
+++ lib/features/home/date/presentation/view/widget/samallerbooklistview.dart
@@ -0,0 +1,26 @@
+import 'package:bookly_app/features/home/date/presentation/view/widget/feturelistviewitem.dart';
+import 'package:flutter/material.dart';
+
+class SimillerListviewBooks extends StatelessWidget {
+  const SimillerListviewBooks({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .2,
+      child: AspectRatio(
+        aspectRatio: 2.6 / 4,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, i) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5),
+              child: Feturelistviewitem(),
+            );
+          },
+        ),
+      ),
+    );
+    ;
+  }
+}
