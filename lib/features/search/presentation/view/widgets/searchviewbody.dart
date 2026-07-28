1947edc95ba61b6656432fdc9c9ef76e12cc24e4
Huda Ramadan <011hodaramadan@gmail.com>
2026-07-27 17:26:41 -0700

    finsh ui

--- lib/features/search/presentation/view/widgets/searchviewbody.dart
+++ lib/features/search/presentation/view/widgets/searchviewbody.dart
@@ -1,3 +1,5 @@
+import 'package:bookly_app/core/utalts/style.dart';
+import 'package:bookly_app/features/home/date/presentation/view/widget/bestsellerlistviewitem.dart';
 import 'package:bookly_app/features/search/presentation/view/widgets/customtextfeild.dart';
 import 'package:flutter/material.dart';
 
@@ -8,7 +10,38 @@ class Searchviewbody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(children: [Customtextfeild()]),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Customtextfeild(),
+          SizedBox(height: 16),
+          Text("Best Seller", style: Style.textstyle18),
+          SizedBox(height: 16),
+          SearchResultlist(),
+        ],
+      ),
     );
+  }
+}
+
+class SearchResultlist extends StatelessWidget {
+  const SearchResultlist({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        // physics: NeverScrollableScrollPhysics(),
+        padding: EdgeInsets.zero,
+        itemCount: 10,
+        itemBuilder: (contxt, i) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: Bestsellerlistviewitem(),
+          );
+        },
+      ),
+    );
+    ;
   }
 }
