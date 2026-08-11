6147be4ed30adb3a571982ea300247de82c9e01d
Huda Ramadan <011hodaramadan@gmail.com>
2026-08-11 06:04:37 -0700

    create Feature book Cubit Methods

---
+++ lib/features/home/date/cubit/home_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:bookly_app/features/home/date/cubit/homeStates.dart';
+import 'package:bookly_app/features/home/date/home_repo/homerepo.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class FeaturebookCubit extends Cubit<HomeBookState> {
+  FeaturebookCubit(this.homerepo) : super(initHomeBook());
+  final Homerepo homerepo;
+  Future<void> fetchHomeBook() async {
+    emit(LoadingHomeBook());
+    var result = await homerepo.fatchfeturebooks();
+    result.fold(
+      (failure) {
+        emit(FailureHomeBook(failure.errormassge));
+      },
+      (books) {
+        emit(SucssesHomeBook(books));
+      },
+    );
+  }
+}
